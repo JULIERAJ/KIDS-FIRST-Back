451d1214d67770abbfb6f06ebab43584bfaa1996
S-chahine <samah_chahine@outlook.com>
2024-07-06 22:27:01 -0400

    modify 'MessageId' to 'messageId' to maintain naming convention

--- models/Album.js
+++ models/Album.js
@@ -1,57 +1,55 @@
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
-const {
-    validateImageURL,
-    validateImageSize,
-} = require('../utils/validators');
+const { validateImageURL, validateImageSize } = require('../utils/validators');
 
 const AlbumSchema = new Schema(
-    {
-        title: {
-            type: String,
-            required: [true, 'The album must have a title'],
-            trim: true,
-        },
-        photos: [
+  {
+    title: {
+      type: String,
+      required: [true, 'The album must have a title'],
+      trim: true,
+    },
+    photos: [
+      {
+        url: {
+          type: String,
+          required: [true, 'Photo URL is required'],
+          validate: [
             {
-                url: {
-                    type: String,
-                    required: [true, 'Photo URL is required'],
-                    validate: [
-                        {
-                            validator: validateImageURL,
-                            message: 'Photo URL must be a valid .jpeg, .jpg, or .png file',
-                        },
-                        {
-                            validator: validateImageSize,
-                            message: 'Photo size must be less than 500 KB',
-                        },
-                    ],
-                },
+              validator: validateImageURL,
+              message:
+                'Photo URL must be a valid .jpeg, .jpg, .png or .pdf file',
             },
-        ],
-        kidID: {
-            type: Schema.Types.ObjectId,
-            ref: 'Kid',
-            required: true,
+            {
+              validator: validateImageSize,
+              message: 'Photo size must be less than 500 KB',
+            },
+          ],
         },
-        createdBy: {
-            type: Schema.Types.ObjectId,
-            ref: 'User',
-            required: true,
-        },
-        MessageId: {
-            type: Schema.Types.ObjectId,
-            ref: 'Message',
-            required: true,
-        },
+      },
+    ],
+    kidID: {
+      type: Schema.Types.ObjectId,
+      ref: 'Kid',
+      required: true,
     },
-    {
-        timestamps: true,
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
     },
+    messageId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Message',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
 );
 
-const Album = mongoose.model('Album', ALbumSchema);
+const Album = mongoose.model('Album', AlbumSchema);
 
 module.exports = Album;
