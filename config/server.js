28e1879a9a35cdbe9ca8293ec4b51ad49041259a
ElenaCherpakova <elena.cherpakova@gmail.com>
2024-07-07 20:08:32 -0400

    move mongodb connection into separate folder

---
+++ config/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config({ path: './.env.local' });
+
+const connectDB = require('./connect');
+
+const { PORT } = process.env;
+const app = require('../index');
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGODB_URI);
+    // eslint-disable-next-line no-console
+    app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+  }
+};
+
+start();
