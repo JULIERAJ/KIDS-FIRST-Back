d111b7978b06ea9934f487c19df5d136c18513d2
Inna Lavrova <inna.v.lavrova@gmail.com>
2024-07-21 22:30:17 -0400

    refactor(models):removing unused models

--- routes/member.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const memberController = require('../controllers/member-controller');
-
-const router = express.Router();
-
-router.post('/member', memberController.memberRegistration);
-
-module.exports = router;
